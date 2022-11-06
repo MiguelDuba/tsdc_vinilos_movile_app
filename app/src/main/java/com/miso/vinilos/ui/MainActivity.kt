353260a000c68a5f3c4638bc8c5d035672e57dca
Ismael Dominguez <ismaeldb_@hotmail.com>
2022-11-06 17:21:34 -0500

    feat: agrega el menu de navegación principal

--- app/src/main/java/com/miso/vinilos/ui/MainActivity.kt
+++ app/src/main/java/com/miso/vinilos/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.miso.vinilos
+package com.miso.vinilos.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.miso.vinilos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
